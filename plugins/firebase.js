0819e1cebef3224c2b8059a725655dfeeb69deb7
047012386z <markloveaw31152@gmail.com>
2024-11-01 17:05:55 +0700

    Your commit message

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,19 +1,21 @@
-// firebase.js
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA1axuHMPpm6-7BDGJ4j0nCEznCHD-wxLk",
-    authDomain: "demowed2test.firebaseapp.com",
-    projectId: "demowed2test",
-    storageBucket: "demowed2test.firebasestorage.app",
-    messagingSenderId: "773525989011",
-    appId: "1:773525989011:web:796f26d01b71cde9688f66",
-    measurementId: "G-CHJ5VPFTNP"
-  };
+  apiKey: "AIzaSyA1axuHMPpm6-7BDGJ4j0nCEznCHD-wxLk",
+  authDomain: "demowed2test.firebaseapp.com",
+  projectId: "demowed2test",
+  storageBucket: "demowed2test.firebasestorage.app",
+  messagingSenderId: "773525989011",
+  appId: "1:773525989011:web:796f26d01b71cde9688f66",
+  measurementId: "G-CHJ5VPFTNP"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-import { db } from '~/src/firebase'; // หากอยู่ใน src
-
+const analytics = getAnalytics(app);
